4d96bd15b422fa58e75b0f5212198078dadd9c34
geatrigger <geatrigger@gmail.com>
2023-11-20 08:38:44 +0900

    Add Directory, Entry, File class

---
+++ src/main/java/Part11Composite/Entry.java
@@ -0,0 +1,18 @@
+package Part11Composite;
+
+public abstract class Entry {
+    public abstract String getName();
+
+    public abstract int getSize();
+
+    public void printList() {
+        printList("");
+    }
+
+    protected abstract void printList(String prefix);
+
+    @Override
+    public String toString() {
+        return getName() + " (" + getSize() + "B)";
+    }
+}
